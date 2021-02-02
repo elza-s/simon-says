cd54897c3457b4148b76516e911d06b0742484ad
elza-s <elza_somu@yahoo.com>
2021-02-02 08:55:45 -0500

    added level changer

--- game.js
+++ game.js
@@ -1,20 +1,60 @@
 let gamePattern = [];
 
 let buttonColours = ["red", "yellow", "green","blue"];
+let userClickedPattern =[]
 
+let level = 0;
+let firstClick = false
+$(document).keydown( function(){
 
-function nextSequence(){
+  if (!firstClick){
+    $('h1').text(`Level ${level}`) ;
+    nextSequence();
+
+    firstClick = true;
+  }
+  
+} 
+)
+  
+  function nextSequence(){
+  level++
+  $('h1').text(`Level ${level}`) ;
+
   let randomNumber = Math.floor(Math.random()*4);
   let randomChosenColour = buttonColours[randomNumber]
   gamePattern.push(randomChosenColour);
 
   $("#"+ randomChosenColour).fadeOut(250).fadeIn(250);
+  playSound(randomChosenColour)
+  animateClick(randomChosenColour)
+  
+}
 
-  let audio = new Audio('./sounds/'+randomChosenColour+'.mp3');
+
+$(".btn").click( function(){
+  let userChosenColour = $(this).attr("id")
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour)
+  animateClick(userChosenColour)
+  
+})
+
+// if (userClickedPattern === gamePattern){
+//   nextSequence()
+// } else {
+//   $('h1').text(`Game Over`) ;
+// }
+
+function playSound(name){
+let audio = new Audio('./sounds/'+name+'.mp3');
   audio.play();
 }
 
-$("button").click( function(event){
-  let userChosenColour = event.attr("id")
-  console.log(userChosenColour);
-})
+function animateClick(currentColour){
+  $('#'+currentColour).addClass("pressed")
+
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
